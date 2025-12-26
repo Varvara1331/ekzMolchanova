586a82f5c4b7ea99078055ac0670ae17bfb2a454
Varvara1331 <molchanchik13@gmail.com>
2025-12-26 10:46:27 +0300

    Реализация ввода данных карты

--- app/ekzApp/Program.cs
+++ app/ekzApp/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//Добавление ветки develop
+﻿using FloydLibrary;
+int n;
+int m;
+double[,] A;
+
+Console.WriteLine("Нахождение кратчайшего пути между точками на карте");
+
+Console.Write("Введите количество точек на карте: ");
+if (int.TryParse(Console.ReadLine(), out n) && n > 0)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("Введите количество путей на карте: ");
+        if (int.TryParse(Console.ReadLine(), out m) && m > 0)
+        {
+            A = new double[n, m];
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write($"Введите расстояние между точкой {i} и точкой {j} (если расстояние не указано, то введите 0: ");
+                if (double.TryParse(Console.ReadLine(), out double aig) && aig > 0)
+                {
+                    A[i, j] = aig;
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка ввода: введите целое положительное число");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка ввода: введите целое положительное число");
+}
