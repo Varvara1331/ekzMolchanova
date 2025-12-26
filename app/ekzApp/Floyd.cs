9c7bb7006b6c40a32170f0ebebae9e0897ad0c2a
Varvara1331 <molchanchik13@gmail.com>
2025-12-26 10:02:27 +0300

    Добавление модуля Floyd

---
+++ app/ekzApp/Floyd.cs
@@ -0,0 +1,19 @@
+﻿namespace FloydLibrary
+{
+    public class FloydClass
+    {
+        public static double[,] Floyd(double[,] a)
+        {
+            int n = a.GetLength(0);
+            double[,] d = new double[n, n];
+            d = (double[,])a.Clone();
+            for (int i = 1; i <= n; i++)
+                for (int j = 0; j <= n - 1; j++)
+                    for (int k = 0; k <= n - 1; k++)
+                        if (d[j, k] > d[j, i - 1] + d[i - 1, k])
+                            d[j, k] = d[j, i - 1] + d[i - 1, k];
+            return d;
+        }
+    }
+}
+}
